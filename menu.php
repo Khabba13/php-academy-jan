2e7029d88cf098f3ded8674e4e3beef9d4b0e986
Habib <khabba13@yandex.ru>
2022-01-23 08:18:17 +0500

    Подробное объяснение приготовления овощной рагу

---
+++ menu.php
@@ -0,0 +1,33 @@
+<?php
+/*
+    Recipe vegetable ragout
+    2 - tablespoons vegetable oil
+    2 - garlic cloves, minced
+    2 - cups onions, chopped
+    2 - medium carrots, sliced in 0.5 pieces
+    4 - medium celery ribs, chopped
+    1, 0.5 - cups cut green beans
+    2 - bay leaves
+    1 - pinch dried thyme
+    1, 0.5 - cups dry red wine
+    1, 0.5 - cups zucchini, sliced
+    1 - lb sliced mushrooms
+    2 - medium potatoes, cut into chunks
+
+*/
+
+    function cookAOvpshnoyRagu(int $oil, int $garlic, int $onion, int $carrot, int $celery, float $beans, float $leave, int $thyme,
+                                float $wine, float $zucchini, int $mushroom, int $potatoe, string $GoodWords)
+            {
+                echo "Heat the oil in a heavy stew pot. Saute the onions, garlic, carrots, celery and green beans for 3-4 minutes.<br>
+                Add the bay leaves, thyme and red wine and boil, uncovered for 3 minutes.<br>
+                Reduce heat, cover, and simmer for 5 minutes.<br>
+                Add the zucchini and mushrooms.<br>
+                Combine the sauce ingredients and then stir the sauce into the vegetables.<br>
+                Simmer about 30 minutes until the vegetables are tender.<br>
+                While the vegetables are stewing, cook the potatoes separately in salted boiling water until they are tender.<br>
+                Drain and add them to the ragout a few minutes before serving.<br>";
+            }
+
+    cookAOvpshnoyRagu(2,2,2,2,4,1,2,1,0.5,1,1,2,'Delicious');
+?>
